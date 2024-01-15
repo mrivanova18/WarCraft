837112ee8dc51d8582187e5c232d1011bd93dd52
mrivanova18 <m.r.ivanova2019@pmg-pernik.com>
2024-01-15 11:12:09 +0200

    create new project

---
+++ WarCraft/WarCraft/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WarCraft.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
