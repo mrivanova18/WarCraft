921c22079b7c11d2b05fb7aadf7a3e132a51c7e0
mrivanova18 <m.r.ivanova2019@pmg-pernik.com>
2024-01-17 10:30:53 +0200

    class product ready

--- WarCraft/WarCraft.Infrastructure/Data/Entities/Product.cs
+++ WarCraft/WarCraft.Infrastructure/Data/Entities/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,40 @@ namespace WarCraft.Infrastructure.Data.Entities
 {
     public class Product
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; } = null!;
+
+        [Required]
+        [MaxLength(40)]
+        public string Type { get; set; } = null!;
+
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(0,2000)]
+        public int QuantityAvailable { get; set; }
+
+        [Required]
+        public int ManufacturerId { get; set; }
+        public virtual Manufacturer Manufacturer { get; set; } = null!;
+
+        [Required]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public string Image { get; set; } = null!;
+
+        public int Discount { get; set; }
+
+        public IEnumerable<Order> Orders { get; set; } = new List<Order>();
     }
 }
