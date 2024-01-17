23cdb17989a496d8f04e20549df6337ad377a82d
mrivanova18 <m.r.ivanova2019@pmg-pernik.com>
2024-01-17 09:21:30 +0200

    category class ready

--- WarCraft/WarCraft.Infrastructure/Data/Entities/Category.cs
+++ WarCraft/WarCraft.Infrastructure/Data/Entities/Category.cs
@@ -8,5 +8,9 @@ namespace WarCraft.Infrastructure.Data.Entities
 {
     public class Category
     {
+        public int Id { get; set; }
+        public string Planes { get; set; } = null!;
+        public string Tanks { get; set; } = null!;
+        public string Ship { get; set; } = null!;
     }
 }
