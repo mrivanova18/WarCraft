1b75e97cb86b5de579fe6d4a71e7ae3f5bec54e2
mrivanova18 <m.r.ivanova2019@pmg-pernik.com>
2024-01-17 09:16:42 +0200

    create classes, manufacturer ready

---
+++ WarCraft/WarCraft.Infrastructure/Data/Entities/Manufacturer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarCraft.Infrastructure.Data.Entities
+{
+    public class Manufacturer
+    {
+        public int Id { get; set; }
+        public string ManufacturerName { get; set; } = null!;
+        public string CountryOfOrigin { get; set; } = null!;
+    }
+}
