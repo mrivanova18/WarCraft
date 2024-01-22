8e6930545f683c16fc357c4477e469ae300112e1
mrivanova18 <m.r.ivanova2019@pmg-pernik.com>
2024-01-22 10:09:52 +0200

    personal order table

---
+++ WarCraft/WarCraft.Infrastructure/Data/Entities/PersonalOrder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarCraft.Infrastructure.Data.Entities
+{
+    public class PersonalOrder
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string UserId { get; set; } = null!;
+        public virtual ApplicationUser ApplicationUser { get; set; } = null!;
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; } = null!;
+        public string NameOfProduct { get; set; } = null!;
+        public string Type { get; set; } = null!;
+        public string Image { get; set; } = null!;
+    }
+}
